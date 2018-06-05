b773a2cff39ab68cacd4cc15270434dcd5ab0165
Eric Dauenhauer <eric@ericyd.com>
2018-06-05 14:09:48 -0700

    update string tests

--- __tests__/string.js
+++ __tests__/string.js
@@ -8,20 +8,20 @@ const util = require('util');
 describe('String', () => {
   test('should return a string', () => {
     const tree = parse(`"mystring";`);
-    expect(tree[0].type).toBe('String');
-    expect(tree[0].value).toBe('mystring');
+    expect(tree[0].expression.type).toBe('String');
+    expect(tree[0].expression.value).toBe('mystring');
   });
 
   test('should allow non-ascii chars in string', () => {
     const tree = parse(`<cfdiv property='√↓►;)┴≡±¡╥-╝åD░║«Y╚á'></cfdiv>`);
-    expect(tree[0].attributes[0].attr.value).toBe('property');
-    expect(tree[0].attributes[0].value.value).toBe('√↓►;)┴≡±¡╥-╝åD░║«Y╚á');
+    expect(tree[0].attributes[0].left.value).toBe('property');
+    expect(tree[0].attributes[0].right.value).toBe('√↓►;)┴≡±¡╥-╝åD░║«Y╚á');
   });
 
   test('should allow /> in string', () => {
     const tree = parse(`"/>";`);
-    expect(tree[0].type).toBe('String');
-    expect(tree[0].value).toBe('/>');
+    expect(tree[0].expression.type).toBe('String');
+    expect(tree[0].expression.value).toBe('/>');
   });
 
   // In test strings, escape characters need to be escaped to emulate
@@ -31,13 +31,13 @@ describe('String', () => {
   // My guess is this is just the way string literals work
   test('should allow escaped double quotes in string', () => {
     const tree = parse(`"test\\"something\\"";`);
-    expect(tree[0].type).toBe('String');
-    expect(tree[0].value).toBe('test\\"something\\"');
+    expect(tree[0].expression.type).toBe('String');
+    expect(tree[0].expression.value).toBe('test\\"something\\"');
   });
 
   test('should allow escaped single quotes in string', () => {
     const tree = parse(`'test\\'something\\'';`);
-    expect(tree[0].type).toBe('String');
-    expect(tree[0].value).toBe("test\\'something\\'");
+    expect(tree[0].expression.type).toBe('String');
+    expect(tree[0].expression.value).toBe("test\\'something\\'");
   });
 });
